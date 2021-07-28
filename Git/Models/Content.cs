425d9254e97fbbe2bd17f521849f096e1fbdc0cb
Alex Znamensky <aznmsky@gmail.com>
2021-07-28 15:18:22 +0300

    AZ-0001. Add Subscription & Content classes

---
+++ Git/Models/Content.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Git
+{
+    public class Content
+    {
+        public Guid ContentId { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Git/Models/Subscription.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Git
+{
+    public class Subscription
+    {
+        public Guid SubscriptionId { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
